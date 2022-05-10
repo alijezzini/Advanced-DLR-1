2356887aa691fe3afe138217f1a66c827afb83cc
Ibrahim Serhane <ibrahim.serhane@montyholding.com>
2022-05-10 12:07:38 +0300

    credentials and source get list

---
+++ DLR/app/Http/Controllers/SourceController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Source;
+use Illuminate\Http\Request;
+
+class SourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function listall()
+    {
+        $source = Source::all();
+        $sender_idblacklist = [
+            'status' => 200,
+            'message' => 'get all sender id black list successfully',
+            'data' => $source,
+        ];
+        return $sender_idblacklist;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
