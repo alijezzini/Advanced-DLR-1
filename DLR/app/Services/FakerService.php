94648ef04625340527f358fa804041f0ae923981
Ibrahim Serhane <ibrahim.serhane@montyholding.com>
2022-05-20 16:57:40 +0300

    Tested and modified FakerService APIs

--- DLR/app/Services/FakerService.php
+++ DLR/app/Services/FakerService.php
@@ -15,45 +15,57 @@ class FakerService
 
     public function __construct(Message $message)
     {
+
         $this->message = $message;
     }
 
     public function checkBlacklistSender(): bool
-    {
+    {           
+
         $source_sender_id = BlacklistSourceRepository::getSourceBySenderId($this->message->sender_id);
 
         if ($source_sender_id->isEmpty()) {
+
             return false;
         } else {
+
             return true;
+            
         }
+
     }
 
     public function checkSenderDestination(): bool
     {
+
         $sender_id_destination = SourceDestinationRepository::getSenderDestination(
             $this->message->sender_id,
             $this->message->destination
         );
+
         if ($sender_id_destination->isEmpty()) {
+ 
+
             return false;
         } else {
             return true;
         }
     }
 
-    public function getTimeDifference(): Carbon
+    public function getTimeDifference()
     {
         $old_destination = SourceDestinationRepository::getSenderDestination(
             $this->message->sender_id,
             $this->message->destination
-        );
+        )[0];
         $old_time_received = Carbon::createFromDate(
             $old_destination->time_received
         );
-        $new_time_received = Carbon::createFromDate($this->message->time_received);
+        $new_time_received = Carbon::createFromDate($this->message->date_received);
 
-        $time_difference = $$old_time_received->diffInDays($new_time_received);
+        $time_difference =$old_time_received->diff($new_time_received)->format('%H:%I:%S');
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $out->writeln($time_difference);
 
         return $time_difference;
     }
@@ -61,9 +73,16 @@ class FakerService
     public function checkFakingInterval(): bool
     {
         $time_difference = $this->getTimeDifference();
-        $time_interval = MessageRepository::getTimeInterval()->time_interval;
+        $time_interval = MessageRepository::getTimeInterval();
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $out->writeln($time_interval);
 
         if ($time_difference > $time_interval) {
+
+            $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+            $out->writeln('d');
+
+
             return true;
         } else {
             return false;
@@ -73,12 +92,14 @@ class FakerService
     public function fakingManager()
     {
         $blacklist_sender = $this->checkBlacklistSender();
+
         if (!$blacklist_sender) {
             // not implemented yet
             MessagesService::sendMessage($this->message);
             return;
         }
         $sender_destination = $this->checkSenderDestination();
+
         if (!$sender_destination) {
             $this->message->fake = 1;
             MessageRepository::updateMessage($this->message);
